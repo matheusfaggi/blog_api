6819c1d6027dfb5290df980096b8adb7ec26ddd7
Matheus Faggi <matheusfaggi.ht@gmail.com>
2019-12-09 00:44:40 -0300

    Using eslint{

--- app/index.js
+++ app/index.js
@@ -1,25 +1,23 @@
-const express = require('express')
-const router = require('./router')
-const { User } = require('./models')
-
-
+const express = require("express");
+const router = require("./router");
+const { User } = require("./models");
 
 class App {
-    constructor(){
-        this.app = express()
+  constructor() {
+    this.app = express();
 
-        this.middlewares()
-        this.routes()
-    }
+    this.middlewares();
+    this.routes();
+  }
 
-    routes(){
-        this.app.use(router)
-    }
+  routes() {
+    this.app.use(router);
+  }
 
-    middlewares(){
-        this.app.use(express.json())
-        this.app.use(express.urlencoded({extended: true}))
-    }
+  middlewares() {
+    this.app.use(express.json());
+    this.app.use(express.urlencoded({ extended: true }));
+  }
 }
 
 module.exports = new App().app;
--- app/router.js
+++ app/router.js
@@ -1,40 +1,36 @@
-const { Router } = require('express')
-const  UserController = require('./controllers/user-controller')
-const  PostController = require('./controllers/post-controller')
-const  LikeController = require('./controllers/like-controller')
+const { Router } = require("express");
+const UserController = require("./controllers/user-controller");
+const PostController = require("./controllers/post-controller");
+const LikeController = require("./controllers/like-controller");
 
+const user = Router();
+user.get("/", UserController.index);
+user.post("/", UserController.store);
+user.get("/:id", UserController.show);
+user.get("/:id/posts", UserController.posts);
+user.put("/:id", UserController.update);
+user.delete("/:id", UserController.delete);
+user.post("/:id/like", UserController.like);
 
+const post = Router();
+post.get("/", PostController.index);
+post.post("/", PostController.store);
+post.get("/:id", PostController.show);
+post.put("/:id", PostController.update);
+post.delete("/:id", PostController.delete);
+post.post("/:id/like", PostController.like);
+post.get("/:id/likes", PostController.likes);
 
-const user = Router()
-user.get('/', UserController.index)
-user.post('/', UserController.store)
-user.get('/:id', UserController.show)
-user.get('/:id/posts', UserController.posts)
-user.put('/:id', UserController.update)
-user.delete('/:id', UserController.delete)
-user.post('/:id/like',UserController.like)
+const like = Router();
+like.get("/", LikeController.index);
+like.post("/", LikeController.store);
+like.get("/:id", LikeController.show);
+like.put("/:id", LikeController.update);
+like.delete("/:id", LikeController.delete);
 
-const post = Router()
-post.get('/', PostController.index)
-post.post('/', PostController.store)
-post.get('/:id', PostController.show)
-post.put('/:id', PostController.update)
-post.delete('/:id', PostController.delete)
-post.post('/:id/like',PostController.like)
-post.get('/:id/likes',PostController.likes)
+const router = Router();
+router.use("/users", user);
+router.use("/posts", post);
+router.use("/likes", like);
 
-
-const like = Router()
-like.get('/', LikeController.index)
-like.post('/', LikeController.store)
-like.get('/:id', LikeController.show)
-like.put('/:id', LikeController.update)
-like.delete('/:id', LikeController.delete)
-
-const router = Router()
-router.use('/users',user)
-router.use('/posts',post)
-router.use('/likes',like)
-
-
-module.exports = router
+module.exports = router;
